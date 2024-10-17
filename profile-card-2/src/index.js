32c6c3b1ce4073f4797e1f35a704f02bb022670c
Khushi Agrawal <kagrawal2016@gmail.com>
2024-10-17 18:59:49 +0530

    commited

---
+++ profile-card-2/src/index.js
@@ -0,0 +1,105 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./styles.css";
+
+const skills = [
+  {
+    skill: "HTML+CSS",
+    level: "advanced",
+    color: "#2662EA",
+  },
+  {
+    skill: "JavaScript",
+    level: "intermediate",
+    color: "#A020F0",
+  },
+  {
+    skill: "Web Design",
+    level: "advanced",
+    color: "#C3DCAF",
+  },
+  {
+    skill: "Git and GitHub",
+    level: "intermediate",
+    color: "#E84F33",
+  },
+  {
+    skill: "React",
+    level: "advanced",
+    color: "#60DAFB",
+  },
+  {
+    skill: "Svelte",
+    level: "beginner",
+    color: "#FF3B00",
+  },
+];
+
+function App() {
+  return (
+    <div className="card">
+      <Avatar />
+      <div className="data">
+        <Intro />
+        {/*Should contain one Skill component
+        for each web dev skill that you have,
+        customized with props */}
+        <SkillList />
+      </div>
+    </div>
+  );
+}
+
+function Avatar() {
+  return <img className="avatar" src="profile.JPG" alt="Khushi" />;
+}
+
+function Intro() {
+  return (
+    <div>
+      <h1>Khushi Agrawal</h1>
+      <p>
+        a Computer Science graduate with hands-on experience in software
+        development from my internship as a Graduate Engineer Trainee at AU
+        Small Finance Bank, where I worked with technologies like Nagios, Active
+        Directory, and Accops. I have a strong passion for Android development
+        and have completed projects in Java, along with solving over 150 Data
+        Structures and Algorithms problems on platforms like LeetCode and Coding
+        Ninja. I'm driven by continuous learning and growth in the development
+        field.
+      </p>
+    </div>
+  );
+}
+
+function SkillList() {
+  return (
+    <div className="skill-list">
+      {skills.map((skill) => (
+        <Skill skill={skill.skill} color={skill.color} level={skill.level} />
+      ))}
+    </div>
+  );
+}
+
+function Skill({ skill, color, level }) {
+  return (
+    <div className="skill" style={{ backgroundColor: color }}>
+      <span>{skill}</span>
+      <span>
+        {level === "beginner" && "👶"}
+        {level === "intermediate" && "💪"}
+        {level === "advanced" && "👍"}
+      </span>
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
